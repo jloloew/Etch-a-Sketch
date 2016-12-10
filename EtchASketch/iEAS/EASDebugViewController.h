108da6945d4823b3f1522fece2f3ccb44dc55294
Justin Loew <jloloew@gmail.com>
2016-12-10 17:11:23 -0600

    Add debug VC (XIB only, not yet connected to code)

---
+++ EtchASketch/iEAS/EASDebugViewController.h
@@ -0,0 +1,13 @@
+//
+//  EASDebugViewController.h
+//  EtchASketch
+//
+//  Created by Justin Loew on 12/10/16.
+//  Copyright © 2016 Justin Loew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EASDebugViewController : UIViewController
+
+@end
---
+++ EtchASketch/iEAS/EASDebugViewController.m
@@ -0,0 +1,37 @@
+//
+//  EASDebugViewController.m
+//  EtchASketch
+//
+//  Created by Justin Loew on 12/10/16.
+//  Copyright © 2016 Justin Loew. All rights reserved.
+//
+
+#import "EASDebugViewController.h"
+
+@interface EASDebugViewController ()
+
+@end
+
+@implementation EASDebugViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
