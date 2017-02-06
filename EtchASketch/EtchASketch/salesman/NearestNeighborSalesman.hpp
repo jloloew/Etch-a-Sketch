0b02e1e64d3b3b76c784e0696056bb4fed0c60e3
Justin Loew <jloloew@gmail.com>
2017-02-06 16:36:55 -0600

    fixup! Make NNSalesman subclass Salesman (oops)

--- EtchASketch/EtchASketch/salesman/NearestNeighborSalesman.hpp
+++ EtchASketch/EtchASketch/salesman/NearestNeighborSalesman.hpp
@@ -16,43 +16,43 @@
 #include "Salesman.hpp"
 
 namespace etchasketch {
-	namespace salesman {
-		
-		/// Solves the traveling salesman problem.
-		class NearestNeighborSalesman : public Salesman {
-			
-		public:
-			/// The startPoint must be contained within the unorderedPoints.
-			NearestNeighborSalesman(const std::unordered_set<KDPoint<2>>
-									&unorderedPoints,
-									const KDPoint<2> &startPoint);
-			
-			virtual ~NearestNeighborSalesman();
-			
-			/// Order the points for the best drawing order.
-			virtual void orderPoints();
-			
-			/// Get a copy of the ordered points.
-			const std::vector<KDPoint<2>> * getOrderedPoints() const
-			{ return new std::vector<KDPoint<2>>(orderedPoints); }
-			
-		protected:
-			const KDPoint<2> startPoint;
-			std::unordered_set<KDPoint<2>> unorderedPoints;
-			std::vector<KDPoint<2>> orderedPoints;
-			
-		private:
-			/**
-			 * Add the nearest neighbor to the last point added until we run out
-			 * of points to add.
-			 *
-			 * @discussion This algorithm produces lots of large lines all the
-			 * way across the image.
-			 */
-			void nearestNeighborAlgorithm(KDTree<2> &kdTree);
-		};
-		
+namespace salesman {
+
+/// Solves the traveling salesman problem.
+class NearestNeighborSalesman : public Salesman {
+
+  public:
+	/// The startPoint must be contained within the unorderedPoints.
+	NearestNeighborSalesman(const std::unordered_set<KDPoint<2>> &unorderedPoints,
+	                        const KDPoint<2> &startPoint);
+
+	virtual ~NearestNeighborSalesman();
+
+	/// Order the points for the best drawing order.
+	virtual void orderPoints();
+
+	/// Get a copy of the ordered points.
+	const std::vector<KDPoint<2>> *getOrderedPoints() const
+	{
+		return new std::vector<KDPoint<2>>(orderedPoints);
 	}
+
+  protected:
+	const KDPoint<2> startPoint;
+	std::unordered_set<KDPoint<2>> unorderedPoints;
+	std::vector<KDPoint<2>> orderedPoints;
+
+  private:
+	/**
+	 * Add the nearest neighbor to the last point added until we run out
+	 * of points to add.
+	 *
+	 * @discussion This algorithm produces lots of large lines all the
+	 * way across the image.
+	 */
+	void nearestNeighborAlgorithm(KDTree<2> &kdTree);
+};
+}
 }
 
 #endif /* NearestNeighborSalesman_hpp */
