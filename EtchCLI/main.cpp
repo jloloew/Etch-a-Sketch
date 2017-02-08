ffacf754d28b8ead98f8209e82dbe33568a82ea2
BlissChapman <bliss@agrible.com>
2017-02-07 22:21:19 -0600

    Added ultra simple Makefile and EtchCLI/main.cpp.

---
+++ EtchCLI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EtchASketch
+//
+//  Created by Bliss Chapman on 2/7/17.
+//  Copyright © 2017 Justin Loew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Welcome to etch ❤️\n");
+    printf("ARGC: %d\n", argc);
+    return 0;
+}
