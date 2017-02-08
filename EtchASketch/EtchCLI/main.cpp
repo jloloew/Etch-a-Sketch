c2a46d7ef48c5d0a8626b8f8c2f3dd7df4f936e8
BlissChapman <bliss@agrible.com>
2017-02-07 23:17:40 -0600

    - Moved EtchCLI out of root dir and into the EtchASketch folder alongside iEAS. - Removed salesman and edgedetect on disk folders so clang can more easily find header files.  Files are still organized under salesman and edgedetect folders within Xcode. - Updated main.cpp to attempt to instantiate etchasketch::Image and etchasketch::ImageFlow objects.

--- EtchASketch/EtchCLI/main.cpp
+++ EtchASketch/EtchCLI/main.cpp
@@ -7,9 +7,14 @@
 //
 
 #include <stdio.h>
+#include "EtchASketch.hpp"
 
 int main(int argc, char *argv[]) {
     printf("Welcome to etch ❤️\n");
     printf("ARGC: %d\n", argc);
+
+    etchasketch::Image input_img = etchasketch::Image(0, 0, NULL);
+    etchasketch::ImageFlow input_img_flow = etchasketch::ImageFlow(input_img);
+
     return 0;
 }
