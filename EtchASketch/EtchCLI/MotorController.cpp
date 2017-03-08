2c7dd80ad72823178670031c6e52244e66388c14
Bliss Chapman <bliss.chapman@gmail.com>
2017-03-08 05:39:53 +0000

    Implemented new MotorController::drawOrderedPoints algorithm.

--- EtchASketch/EtchCLI/MotorController.cpp
+++ EtchASketch/EtchCLI/MotorController.cpp
@@ -8,6 +8,8 @@
 
 #include "MotorController.hpp"
 #include <iostream>
+#include <math.h>
+#include <unistd.h>
 
 MotorController::MotorController()
 {
@@ -25,7 +27,7 @@ MotorController::MotorController()
     // Initialize motor nibLoc
     nibLoc.x = 0;
     nibLoc.y = 0;
-    
+
     if (motor_init(&motorX) || motor_init(&motorY)) {
         fprintf(stderr, "Error creating motor.\n");
         exit(1);
@@ -34,104 +36,91 @@ MotorController::MotorController()
 
 void MotorController::drawOrderedPoints(const std::vector<etchasketch::KDPoint<2>> &points)
 {
-    cout << "DRAWING ORDERED EDGE POINTS" << endl;
-    
-    // for each point to draw
-    for(size_t i = 0; i < points.size(); i++) {
-        /*
-        etchasketch::KDPoint<2> target = points[i];
+  // for each point to draw
+  for(size_t i = 0; i < points.size(); i++) {
+    etchasketch::KDPoint<2> goal_pt = points[i];
+    etchasketch::KDPoint<2> start_pt = etchasketch::KDPoint<2>(nibLoc.x, nibLoc.y);
+    etchasketch::KDPoint<2> current_pt = start_pt;
 
-        int x_dist = target[0] - nibLoc.x;
-        int y_dist = target[1] - nibLoc.y;
+    // while not at goal_pt
+    while((current_pt[0] != goal_pt[0]) || (current_pt[1] != goal_pt[1])) {
 
-        // While not at target…
-        while (x_dist || y_dist) {
-            if (x_dist < 0) {
-                motor_prepare_move(&motorX, DIR_CCW);
-                x_dist++;
-                nibLoc.x--;
-            } else if (x_dist > 0) {
-                motor_prepare_move(&motorX, DIR_CW);
-                x_dist--;
-                nibLoc.x++;
-            }
+      // enumerate possible next coordinates
+      etchasketch::KDPoint<2> n, e, s, w;
+      n = current_pt; e = current_pt; s = current_pt; w = current_pt;
 
-            if (y_dist < 0) {
-                motor_prepare_move(&motorY, DIR_CCW);
-                y_dist++;
-                nibLoc.y--;
-            } else if (y_dist > 0) {
-                motor_prepare_move(&motorY, DIR_CW);
-                y_dist--;
-                nibLoc.y++;
-            }
-            
-            motor_execute_move(&motorX, 2);
-        }
-        */
+      if (current_pt[1] > 0) n[1]--;
+      if (current_pt[0] > 0) e[0]--;
+      if (current_pt[1] < EAS_BOARD_HEIGHT) s[1]++;
+      if (current_pt[0] < EAS_BOARD_WIDTH) w[0]++;
 
-        etchasketch::KDPoint<2> tgt_pt = points[i];
-        float slope = (tgt_pt[1] - nibLoc.y)/(tgt_pt[0] - nibLoc.x);
-        
-        // while not at target point
-        while (nibLoc.x != tgt_pt[0] && nibLoc.y != tgt_pt[1]) {
-            
-            cout << "x: " << nibLoc.x << ", y: " << nibLoc.y << endl;
+      // calculate distance from possible choices to goal_pt
+      double curr_dist, n_dist, e_dist, s_dist, w_dist;
+      curr_dist = euclidean_distance(current_pt, goal_pt);
+      n_dist = euclidean_distance(n, goal_pt);
+      e_dist = euclidean_distance(e, goal_pt);
+      s_dist = euclidean_distance(s, goal_pt);
+      w_dist = euclidean_distance(w, goal_pt);
 
-            // mark position along slope line in proper x direction
-            float slope_pt_x = nibLoc.x;
-            float slope_pt_y = nibLoc.y;
+      // generate chosen_pt that minimizes distance to goal_pt
+      etchasketch::KDPoint<2> chosen_pt = current_pt;
 
-            // mark unit movement position in proper x and y direction
-            float ref_pt_x = nibLoc.x;
-            float ref_pt_y = nibLoc.y;
+      // X
+      if (e_dist < curr_dist || w_dist < curr_dist) {
+        if (e_dist < w_dist)
+          chosen_pt[0] = e[0];
+        else
+          chosen_pt[0] = w[0];
+      }
 
-            // update markers...
-            if (nibLoc.x < tgt_pt[0]) {
-                slope_pt_x++;
-                slope_pt_y += slope;
-                
-                ref_pt_x++;
-            }
-            if (nibLoc.x > tgt_pt[0]) {
-                slope_pt_x--;
-                slope_pt_y -= slope;
-                
-                ref_pt_x--;
-            }
-            
-            if (nibLoc.y < tgt_pt[1]) {
-                ref_pt_y++;   
-            }
-            if (nibLoc.y > tgt_pt[1]) {
-                ref_pt_y--;
-            }
+      // Y
+      if (n_dist < curr_dist || s_dist < curr_dist) {
+        if (n_dist < s_dist)
+          chosen_pt[1] = n[1];
+        else
+          chosen_pt[1] = s[1];
+      }
 
-            // choose x or y movement
-            if (slope_pt_y >= ref_pt_y) {         // y movement
-                if (tgt_pt[1] > nibLoc.y) {
-                    motor_move(&motorY, DIR_CW);
-                    nibLoc.y++;
-                }
-                if (tgt_pt[1] < nibLoc.y) {
-                    motor_move(&motorY, DIR_CCW);
-                    nibLoc.y--;
-                }
-            } else {                             // x movement
-                if (tgt_pt[0] > nibLoc.x) {
-                    motor_move(&motorX, DIR_CW);
-                    nibLoc.x++;
-                }
-                if (tgt_pt[0] < nibLoc.x) {
-                    motor_move(&motorX, DIR_CCW);
-                    nibLoc.x--;
-                }
-            }
-        }
-    }
+      // move to chosen point
+      moveToPoint(chosen_pt);
+      current_pt = chosen_pt;
+
+      // debugging
+      // cout << "n x: " << n[0] << ", y: " << n[1] << endl;
+      // cout << "     n_dist:" << n_dist << endl;
+      // cout << "s x: " << s[0] << ", y: " << s[1] << endl;
+      // cout << "     s_dist:" << s_dist << endl;
+      // cout << "e x: " << e[0] << ", y: " << e[1] << endl;
+      // cout << "     e_dist:" << e_dist << endl;
+      // cout << "w x: " << w[0] << ", y: " << w[1] << endl;
+      // cout << "     w_dist:" << w_dist << endl;
+      //
+      // cout << "chosen_pt x: " << chosen_pt[0] << ", y: " << chosen_pt[1] << endl;
+      // cout << "goal_pt x: " << goal_pt[0] << ", y: " << goal_pt[1] << endl;
+      // cout << "----------" << endl;
+      cout << "current_pt x: " << current_pt[0] << ", y: " << current_pt[1] << endl;
+      // cout << "----------" << endl << endl;
+      //
+      // sleep(1);
+
+    } // while not at goal_pt
+  } // for each point
+}
+
+double MotorController::euclidean_distance(const etchasketch::KDPoint<2> &a,
+                                           const etchasketch::KDPoint<2> &b)
+{
+  double x = a[0] - b[0];
+  double y = a[1] - b[1];
+  return sqrt(pow(x, 2) + pow(y, 2));
 }
 
 int MotorController::moveToPoint(const etchasketch::KDPoint<2> &pt)
 {
+    nibLoc.x = pt[0];
+    nibLoc.y = pt[1];
+
+    // control motors!
+
     return -1;
 }
