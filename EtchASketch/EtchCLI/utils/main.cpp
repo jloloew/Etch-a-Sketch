7f1f3f7e2e61c24b39e5db5e5706ab1fcbd49dea
BlissChapman <bliss@agrible.com>
2017-03-09 20:10:51 -0600

    Updated EtchCLI/utils/main.cpp with the foundations of taking arguments as input and output files.

--- EtchASketch/EtchCLI/utils/main.cpp
+++ EtchASketch/EtchCLI/utils/main.cpp
@@ -1,10 +1,41 @@
+#include <stdio.h>
 #include "image.h"
 
-int main()
+using namespace std;
+
+/*
+void usage() {
+    return;
+}
+*/
+
+int
+main(int argc, char * const argv[])
 {
-	Image* lena = new Image();
-	lena->readFromFile("lena.png");
-	lena->etchToFile("lena.etch");
+    cout << "Welcome to the etcher utility, the one and only .etch file generator!" << endl;
+
+    // Parse arguments.
+    /*
+    string inFile;
+    long imgWidth = -1, imgHeight = -1;
+    int ch;
+    while ((ch = getopt(argc, argv, "i")) != -1) {
+        switch (ch) {
+            case 'i':
+                inFile = string(optarg);
+                break;
+            case '?':
+            default:
+                usage();
+        }
+    }
+    */
+
+	Image* imageToEtch = new Image();
+	imageToEtch->readFromFile("lena.png");
+	imageToEtch->etchToFile("lena.etch");
+
+    cout << "I have written your etch file to lena.etch 🎉" << endl;
 
 	return 0;
 }
