4b91c38dbf93adc36d2fbfd5255f03c698161b18
Justin + Bliss <jloloew@gmail.com>
2017-03-14 00:42:57 +0000

    Add ability to specify how many steps a motor util should move before exiting

--- EtchASketch/EtchCLI/motor-main.c
+++ EtchASketch/EtchCLI/motor-main.c
@@ -1,11 +1,22 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "motor.h"
 
-int
-main(int argc, char *argv[])
+static void __attribute__((noreturn))
+usage(const char *argv[])
 {
-    printf("Setting up...\n");
+    printf("Usage: %s [num-steps]\n", argv[0]);
+    exit(1);
+}
+
+int
+main(int argc, const char *argv[])
+{
+    if (argc > 2) {
+        usage(argv);
+    }
+    
     motor_initialize();
     
     // Set up a motor.
@@ -13,6 +24,15 @@ main(int argc, char *argv[])
     if (motor_init(&m[0]) || motor_init(&m[1])) {
         fprintf(stderr, "Error creating motor.\n");
         return 1;
+    }
+
+    // Get the number of steps to perform.
+    unsigned long num_steps = 0;
+    if (argc > 1) {
+        num_steps = strtoul(argv[1], NULL, 10);
+        if (num_steps == 0) {
+            usage(argv);
+        }
     }
 
     // Print the pin numbers.
@@ -26,8 +46,20 @@ main(int argc, char *argv[])
 	
 //    print_gpio_labels();
 
+    if (num_steps > 0) {
+        printf("Will move %lu steps\n", num_steps);
+    }
+    
     printf("Beginning rotation...\n");
-    while (1) {
+    
+    int keep_going = 1;
+    while (keep_going) {
+        // If the user specified num_steps, stop eventually. Otherwise, loop
+        // forever.
+        if (num_steps > 0) {
+            keep_going = --num_steps > 0;
+        }
+        
 #ifdef MOTOR_MOVE_LEFT
         motor_prepare_move(&m[0], DIR_CCW);
 #elif defined MOTOR_MOVE_RIGHT
