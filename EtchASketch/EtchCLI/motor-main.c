d9a43c0b17df64173b57a29db195b08043982f30
Justin Loew <jloloew@gmail.com>
2017-03-13 19:04:53 -0500

    Move motor utils into EtchCLI directory and Makefile

--- EtchASketch/EtchCLI/motor-main.c
+++ EtchASketch/EtchCLI/motor-main.c
@@ -17,15 +17,28 @@ main(int argc, char *argv[])
 
     // Print the pin numbers.
     printf("WiringPi pin numbers:\n");
-    printf("Step:      %02d\n", m[0].pin_step);
-    printf("Direction: %02d\n", m[0].pin_dir);
-    
+	printf("Motor 0:\n");
+    printf("    Step:      %02d\n", m[0].pin_step);
+	printf("    Direction: %02d\n", m[0].pin_dir);
+	printf("Motor 1:\n");
+	printf("    Step:      %02d\n", m[1].pin_step);
+	printf("    Direction: %02d\n", m[1].pin_dir);
+	
 //    print_gpio_labels();
 
     printf("Beginning rotation...\n");
     while (1) {
+#ifdef MOTOR_MOVE_LEFT
         motor_prepare_move(&m[0], DIR_CCW);
-        //motor_prepare_move(&m[1], DIR_CCW);
+#elifdef MOTOR_MOVE_RIGHT
+		motor_prepare_move(&m[0], DIR_CW);
+#endif
+		
+#ifdef MOTOR_MOVE_DOWN
+		motor_prepare_move(&m[1], DIR_CCW);
+#elifdef MOTOR_MOVE_UP
+		motor_prepare_move(&m[1], DIR_CW);
+#endif
         motor_execute_move(m, 2);
     }
 
