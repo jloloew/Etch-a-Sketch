8aa95c024b36e32b4d84abb4f9d0a5f317b93e89
Justin Loew <jloloew@gmail.com>
2017-03-16 12:25:19 -0500

    Add a printed message with the total number of steps taken when motor-{dir} is killed (untested)

--- EtchASketch/EtchCLI/motor-main.c
+++ EtchASketch/EtchCLI/motor-main.c
@@ -1,7 +1,18 @@
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "motor.h"
+
+static unsigned long num_steps_taken = 0;
+
+static void
+int_handler(int sig __attribute__((unused)))
+{
+    printf("\n\nTotal number of steps taken: %lu\n", num_steps_taken);
+    // Just exit.
+    _exit(0);
+}
 
 static void __attribute__((noreturn))
 usage(const char *argv[])
@@ -16,10 +27,10 @@ main(int argc, const char *argv[])
     if (argc > 2) {
         usage(argv);
     }
-    
+
     motor_initialize();
-    
-    // Set up a motor.
+
+    // Set up the motors.
     motor_t m[2];
     if (motor_init(&m[0]) || motor_init(&m[1])) {
         fprintf(stderr, "Error creating motor.\n");
@@ -43,15 +54,20 @@ main(int argc, const char *argv[])
 	printf("Motor 1:\n");
 	printf("    Step:      %02d\n", m[1].pin_step);
 	printf("    Direction: %02d\n", m[1].pin_dir);
-	
+
 //    print_gpio_labels();
 
     if (num_steps > 0) {
         printf("Will move %lu steps\n", num_steps);
     }
-    
+
+    // Install signal handler to print the total number of steps taken at exit.
+    struct sigaction act;
+    act.sa_handler = int_handler;
+    sigaction(SIGINT, &act, NULL);
+
     printf("Beginning rotation...\n");
-    
+
     int keep_going = 1;
     while (keep_going) {
         // If the user specified num_steps, stop eventually. Otherwise, loop
@@ -59,21 +75,22 @@ main(int argc, const char *argv[])
         if (num_steps > 0) {
             keep_going = --num_steps > 0;
         }
-        
+
 #ifdef MOTOR_MOVE_LEFT
         motor_prepare_move(&m[0], DIR_CCW);
 #elif defined MOTOR_MOVE_RIGHT
 		motor_prepare_move(&m[0], DIR_CW);
 #endif
-		
+
 #ifdef MOTOR_MOVE_DOWN
 		motor_prepare_move(&m[1], DIR_CCW);
 #elif defined MOTOR_MOVE_UP
 		motor_prepare_move(&m[1], DIR_CW);
 #endif
         motor_execute_move(m, 2);
+
+        num_steps_taken++;
     }
 
     return 0;
 }
-
