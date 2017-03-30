ffa6556a1babf78336b496421849e9ffb092572c
Justin Loew <jloloew@gmail.com>
2017-03-29 22:37:11 -0500

    Reorder ctor initialization ordering in rgbapixel.cpp to fix build warning

--- EtchASketch/EtchCLI/utils/rgbapixel.cpp
+++ EtchASketch/EtchCLI/utils/rgbapixel.cpp
@@ -7,19 +7,19 @@
  */
 #include "rgbapixel.h"
 
-RGBAPixel::RGBAPixel() : red(255), green(255), blue(255), alpha(255)
+RGBAPixel::RGBAPixel() : alpha(255), blue(255), green(255), red(255)
 {
 	/* nothing */
 }
 
 RGBAPixel::RGBAPixel(uint8_t r, uint8_t g, uint8_t b)
-	: red(r), green(g), blue(b), alpha(255)
+	: alpha(255), blue(b), green(g), red(r)
 {
 	/* nothing */
 }
 
 RGBAPixel::RGBAPixel(uint8_t r, uint8_t g, uint8_t b,
-		             uint8_t a) : red(r), green(g), blue(b), alpha(a)
+		             uint8_t a) : alpha(a), blue(b), green(g), red(r)
 {
 	/* nothing */
 }
